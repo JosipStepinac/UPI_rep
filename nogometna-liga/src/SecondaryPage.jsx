f1a854247d5332ee875e91c3d7bd056b12f13627
JosipStepinac <jstepinac094@gmail.com>
2025-01-25 14:30:48 +0100

    Add files via upload

--- nogometna-liga/src/SecondaryPage.jsx
+++ nogometna-liga/src/SecondaryPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import { auth } from /config/firebase-configClubs;
-import { onAuthStateChanged } from firebase/auth;
+import { auth } from './config/firebase-config';
+import { onAuthStateChanged } from 'firebase/auth';
 import './SecondaryPage.css';
 import logo from './assets/logo.png';
 
