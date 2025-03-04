c8eba3663b6aa2fc2c3e5a0ce811e38fc676956b
mmiskicc <mmiskic@pmfst.hr>
2025-03-04 21:34:03 +0100

    Update jest.config.cjs

--- nogometna-liga/jest.config.cjs
+++ nogometna-liga/jest.config.cjs
@@ -1,5 +1,12 @@
 module.exports = {
   moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(css|less|sass|scssjpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
   },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
 };
+
